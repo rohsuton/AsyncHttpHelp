0632ea4bda08a6f9399a90bffaa7d10bc5d8257d
罗旭东 <rohsuton@gmail.com>
2017-04-17 01:48:24 -0500

    解决UserAgent带中文时异常的问题

--- src/main/src/main/java/com/luoxudong/app/asynchttp/interceptor/UserAgentInterceptor.java
+++ src/main/src/main/java/com/luoxudong/app/asynchttp/interceptor/UserAgentInterceptor.java
@@ -9,6 +9,9 @@
  */
 package com.luoxudong.app.asynchttp.interceptor;
 
+import android.os.Build;
+import android.webkit.WebSettings;
+
 import java.io.IOException;
 
 import com.luoxudong.app.asynchttp.AsyncHttpConst;
@@ -43,8 +46,26 @@ public class UserAgentInterceptor implements Interceptor {
         Request request = chain.request();
         Request newRequest = request.newBuilder()
         .removeHeader(AsyncHttpConst.HEADER_USER_AGENT)
-        .addHeader(AsyncHttpConst.HEADER_USER_AGENT, mUserAgent)
+        .addHeader(AsyncHttpConst.HEADER_USER_AGENT, checkUserAgent(mUserAgent))
         .build();
         return chain.proceed(newRequest);
+    }
+
+    /**
+     * 对不合法字符处理，见Headers->checkNameAndValue
+     * @param userAgent
+     * @return
+     */
+    private String checkUserAgent(String userAgent) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0, length = userAgent.length(); i < length; i++) {
+            char c = userAgent.charAt(i);
+            if (c <= '\u001f' && c!='\u0009' || c >= '\u007f') {
+                sb.append(String.format("\\u%04x", (int) c));
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 }
